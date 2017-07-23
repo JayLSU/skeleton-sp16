c1ccf8ed836064ebad351e4916f8dfb6b0bc6da7
Admin <you@example.com>
2017-07-22 23:54:11 -0500

    finish proj1

--- proj1/ArrayDeque.java
+++ proj1/ArrayDeque.java
@@ -3,21 +3,23 @@ public class ArrayDeque<Blorp>{
 	private int size;
 	private int front;
 	private int back;
-	private int remainedLen;
+	private int resizeFactor;
+	private double usageFactor;
 
 	public ArrayDeque(){
 		items = (Blorp []) new Object[8];
 		size = 0;
 		front = 0;
 		back = 1;
-		remainedLen = 8;
+		resizeFactor = 2;
+		usageFactor = 0.25;
 	}
 
 	// addOne method: Adds one to an index
 	public int addOne(int index){
 		int reminder = (index+1);
-		if (reminder >= (size + remainedLen)) {
-			reminder = reminder - (size + remainedLen);
+		if (reminder >= items.length) {
+			reminder = reminder - items.length;
 		}
 		return reminder;
 	}
@@ -26,7 +28,7 @@ public class ArrayDeque<Blorp>{
 	public int minusOne(int index){
 		int reminder = (index-1);
 		if (reminder < 0) {
-			reminder = (size + remainedLen) + reminder;
+			reminder = items.length + reminder;
 		}
 		return reminder;
 	}
@@ -34,18 +36,24 @@ public class ArrayDeque<Blorp>{
 
 	// addFirst method: Adds an item to the front of the Deque
 	public void addFirst(Blorp x){
+		if (items.length == size) {
+			this.resize();
+		}
+
 		this.items[front] = x;
 		front = this.minusOne(front);
 		size += 1;
-		remainedLen -= 1;
 	}
 
 	// addLast method: put an item to the back of the Deque
 	public void addLast(Blorp x){
+		if (items.length == size) {
+			this.resize();
+		}
+
 		this.items[back] = x;
 		back = this.addOne(back);
 		size += 1;
-		remainedLen -= 1;
 	}
 
 	// isEmpty method: Returns true if Deque is empty, false otherwise
@@ -56,9 +64,61 @@ public class ArrayDeque<Blorp>{
 		return false;
 	}
 
+	// isFull method: Returns true if Deque is full, false otherwise
+	public boolean isFull(){
+		if (size == items.length) {
+			return true;
+		}
+		return false;
+	}
+
 	// size method: Returns the number of itmes in the Deque
 	public int size(){
 		return this.size;
+	}
+
+	// doublesize method:
+	public void doublesize(){
+		int newsize = resizeFactor * size;
+		int newstartposition;
+		int frontlength;
+		Blorp[] temp = (Blorp []) new Object[newsize];
+		if(back == 0){
+			System.arraycopy(items,0,temp,0,size);
+			items = temp;
+			back = size;
+			front = this.minusOne(0);
+		}else{
+			newstartposition = newsize - items.length + back;
+			frontlength = items.length - back;
+			System.arraycopy(items,0,temp,0,back);
+			System.arraycopy(items,back,temp,newstartposition,frontlength);
+			items = temp;
+			front = this.minusOne(newstartposition);
+		}
+	}
+
+	// halfsize method:
+	public void halfsize(){
+		int newsize = items.length / resizeFactor;
+		int cpPos = this.addOne(front);
+		Blorp[] temp = (Blorp []) new Object[newsize];
+		for(int i=1;i <= this.size();i++){
+			temp[i] = this.items[cpPos];
+			cpPos = this.addOne(cpPos);
+		}
+		items = temp;
+		front = 0;
+		back = size + 1;
+	}
+
+	// resize method: Resize the ArrayDeque to proper size
+	public void resize(){
+		if(this.isFull()){
+			this.doublesize();
+		}else{
+			this.halfsize();
+		}
 	}
 
 	// printDeque method: Prints the items in the Deque from first to last, separated by a space.
@@ -67,7 +127,7 @@ public class ArrayDeque<Blorp>{
 			System.out.print("The ArrayDeque is EMPTY!");
 		}else{
 			System.out.print("The ArrayDeque is: ");
-			for (int i = 0; i < (this.size + this.remainedLen); i++ ) {
+			for (int i = 0; i < items.length; i++ ) {
 				System.out.print( this.get(i) + " ");
 			}
 		}
@@ -76,11 +136,15 @@ public class ArrayDeque<Blorp>{
 
 	// removeFirst method: Removes and retruns the item at the front of the Deque. If no such item exists, returns null.
 	public Blorp removeFirst(){
+		double currentusage;
+		currentusage = (double)this.size()/items.length;
 		if (this.isEmpty()) {
 			return null;
 		}
+		if(currentusage <= usageFactor && items.length > 8){
+			this.resize();
+		}
 		size -= 1;
-		remainedLen += 1;
 		front = this.addOne(front);
 		this.items[front] = null;
 		return this.items[addOne(front)];		
@@ -88,11 +152,14 @@ public class ArrayDeque<Blorp>{
 
 	// removeLast method: Removes and retruns the item at the back of the Deque. If no such item exists, returns null.
 	public Blorp removeLast(){
+		double currentusage = (double)this.size()/items.length;
 		if (this.isEmpty()) {
 			return null;
 		}
+		if(currentusage <= usageFactor && items.length > 8){
+			this.resize();
+		}
 		size -= 1;
-		remainedLen += 1;
 		back = this.minusOne(back);
 		this.items[back] = null;
 		return this.items[minusOne(back)];	
@@ -101,38 +168,46 @@ public class ArrayDeque<Blorp>{
 	// get mehod: Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth. If no
 	// such item exists, returns null. Must not alter the Deque!
 	public Blorp get(int index){
-		if (index > (size + remainedLen - 1)) {
+		if (index > (items.length - 1)) {
 			return null;
 		}
 		return this.items[index];
 	}
 
-
-	
-	public static void main(String[] args) {
-		ArrayDeque<String> A1 = new ArrayDeque<String>();
-		A1.printDeque();
-		A1.addLast("a");
-		A1.addLast("b");
-		A1.addLast("c");
-		A1.addLast("d");
-		A1.removeFirst();
-		A1.removeFirst();
-		A1.removeFirst();
-		A1.removeFirst();
-		System.out.println("Front is " + A1.front + ". Back is " + A1.back);
-		A1.addLast("a");
-		System.out.println("Front is " + A1.front + ". Back is " + A1.back);
-		A1.addLast("b");
-		System.out.println("Front is " + A1.front + ". Back is " + A1.back);
-		A1.addFirst("c");
-		System.out.println("Front is " + A1.front + ". Back is " + A1.back);
-		A1.addLast("d");
-		System.out.println("Front is " + A1.front + ". Back is " + A1.back);
-		A1.addLast("e");
-		System.out.println("Front is " + A1.front + ". Back is " + A1.back);
-		A1.printDeque();
-		System.out.println(A1.get(5));
+	// getlength method: Returns the length of ArrayDeque
+	public int getlength(){
+		return items.length;
 	}
+	
+	/*public static void main(String[] args) {
+		ArrayDeque<Integer> A1 = new ArrayDeque<Integer>();
+		A1.addFirst(0);
+		for(int i = 1 ; i <16;i++){
+			A1.addLast(i);
+		}
+		A1.printDeque();
+		System.out.println("front is " + A1.front + " back is " + A1.back);
+		for(int j = 1;j<5;j++) {
+			A1.removeFirst();
+		}
+
+		for(int j = 1;j<4;j++) {
+			A1.removeLast();
+		}
+		A1.printDeque();
+		System.out.println("front is " + A1.front + " back is " + A1.back);
+
+		for(int i = 1 ; i <10;i++){
+			A1.addLast(i);
+		}
+		A1.printDeque();
+		System.out.println("front is " + A1.front + " back is " + A1.back);
+
+		for(int i = 1 ; i <12;i++){
+			A1.removeFirst();
+		}
+		A1.printDeque();
+		System.out.println("front is " + A1.front + " back is " + A1.back);
+	}*/
 	
 }
