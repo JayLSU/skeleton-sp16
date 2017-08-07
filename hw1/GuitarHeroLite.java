a9be5fe9cc9bbdc52ed48209dc4404f46c95a205
Admin <you@example.com>
2017-08-07 01:56:44 -0500

    finish hw1 just for fun part

--- hw1/GuitarHeroLite.java
+++ hw1/GuitarHeroLite.java
@@ -1,34 +1,95 @@
 /** A client that uses the synthesizer package to replicate a plucked guitar string sound */
 public class GuitarHeroLite {
-    private static final double CONCERT_A = 440.0;
-    private static final double CONCERT_C = CONCERT_A * Math.pow(2, 3.0 / 12.0);
+
+    // this part is for guitar
+    /**************************************************************************/
+    /*private static final double CONCERT_A = 440.0;
+    private static final double CONCERT_C = CONCERT_A * Math.pow(2, 3.0 / 12.0);*/
+    /**************************************************************************/
+
+    // this part is for keyboard
+    /**************************************************************************/
+    private static final String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+    /**************************************************************************/
 
     public static void main(String[] args) {
+        // this part is for guitar
+        /**************************************************************************/
         /* create two guitar strings, for concert A and C */
-        synthesizer.GuitarString stringA = new synthesizer.GuitarString(CONCERT_A);
-        synthesizer.GuitarString stringC = new synthesizer.GuitarString(CONCERT_C);
+        /*synthesizer.GuitarString stringA = new synthesizer.GuitarString(CONCERT_A);
+        synthesizer.GuitarString stringC = new synthesizer.GuitarString(CONCERT_C);*/
+        /**************************************************************************/
+
+        // this part is for keyboard
+        /**************************************************************************/
+        /* create 37 keyboard array and initialize each with corresponding frequency*/
+        synthesizer.GuitarString[] Pianokeyboard = new synthesizer.GuitarString[37];
+        for (int i =0; i < 37; i++){
+            double freq = 440 * Math.pow(2,(i - 24)/12);
+            Pianokeyboard[i] = new synthesizer.GuitarString(freq);
+        }
+        /**************************************************************************/
+
 
         while (true) {
 
             /* check if the user has typed a key; if so, process it */
             if (StdDraw.hasNextKeyTyped()) {
                 char key = StdDraw.nextKeyTyped();
-                if (key == 'a') {
+                // this part is for guitar
+                /**************************************************************************/
+                /*if (key == 'a') {
                     stringA.pluck();
                 } else if (key == 'c') {
                     stringC.pluck();
+                }*/
+                /**************************************************************************/
+
+
+                // this part is for keyboard
+                /**************************************************************************/
+                if (keyboard.contains( Character.toString(key))) { // make sure no crash on unexpected key press
+                    // keyboard play
+                    int keyInd = keyboard.indexOf(key);
+                    double fre = 440 * Math.pow(2, (keyInd - 24) / 12);
+                    Pianokeyboard[keyInd] = new synthesizer.GuitarString(fre);
+                    Pianokeyboard[keyInd].pluck();
                 }
+                /**************************************************************************/
             }
 
         /* compute the superposition of samples */
-            double sample = stringA.sample() + stringC.sample();
+            // this is for guitar
+            /**************************************************************************/
+            /* double sample = stringA.sample() + stringC.sample(); */
+            /**************************************************************************/
+
+            // this part is for keyboard
+            /**************************************************************************/
+            double sample = 0.0;
+            for (int j = 0; j < 37; j++){
+                sample += Pianokeyboard[j].sample();
+            }
+            /**************************************************************************/
+
 
         /* play the sample on standard audio */
             StdAudio.play(sample);
 
         /* advance the simulation of each guitar string by one step */
-            stringA.tic();
-            stringC.tic();
+
+            // this part is for guitar
+            /**************************************************************************/
+            /*stringA.tic();
+            stringC.tic();*/
+            /**************************************************************************/
+
+            // this part is for keyboard
+            /**************************************************************************/
+            for (int i =0; i < 37; i++){
+                Pianokeyboard[i].tic();
+            }
+            /**************************************************************************/
         }
     }
 }
---
+++ hw1/readme.md
@@ -0,0 +1,7 @@
+Here is a simple instruction.
+
+Current GuitarHeroLite.java builds a 37 key synthesizer, which is ["Just For Fun" part in hw1 of UCB cs61b sp16](http://datastructur.es/sp16/materials/hw/hw1/hw1.html#just-for-fun-building-a-37-key-synthesizer).
+
+If needed, anyone can uncomment guitar part and comment keyboard part to obtain orginal hw1 solutions for Task 1 to Task 5. "Even More Fun" part is not implemented. Everyone can try it by yourself. 
+
+Have fun! :)
