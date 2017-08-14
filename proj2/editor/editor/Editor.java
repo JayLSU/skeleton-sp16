d1b59b4ab7d2877f89ca7175c371cca3ce0a76d5
Admin <you@example.com>
2017-08-13 23:46:51 -0500

    create KeyEventHandler class in sg package including ShortcutKey event

--- proj2/editor/editor/Editor.java
+++ proj2/editor/editor/Editor.java
@@ -13,108 +13,12 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import java.util.LinkedList;
 import sg.util.Print;
+import sg.util.KeyEventHandler;
 
 public class Editor extends Application {
     private static final int WINDOW_WIDTH = 500;
     private static final int WINDOW_HEIGHT = 500;
     private static final int MARGIN = 5;
-
-    /** An EventHandler to handle keys that get pressed. */
-    private class KeyEventHandler implements EventHandler<KeyEvent> {
-        int textCenterX;
-        int textCenterY;
-
-        private static final int STARTING_FONT_SIZE = 20;
-        private static final int STARTING_TEXT_POSITION_X = 0;
-        private static final int STARTING_TEXT_POSITION_Y = 0;
-        private LinkedList<String > allToDisplay = new LinkedList<>();
-        /** The Text to display on the screen. */
-        private Text displayText = new Text(STARTING_TEXT_POSITION_X, STARTING_TEXT_POSITION_Y, "");
-        private int fontSize = STARTING_FONT_SIZE;
-
-        private String fontName = "Verdana";
-
-        KeyEventHandler(final Group root, int windowWidth, int windowHeight) {
-            textCenterX = 0;
-            textCenterY = 0;
-            // Initialize some empty text and add it to root so that it will be displayed.
-            displayText = new Text(textCenterX, textCenterY, "");
-            // Always set the text origin to be VPos.TOP! Setting the origin to be VPos.TOP means
-            // that when the text is assigned a y-position, that position corresponds to the
-            // highest position across all letters (for example, the top of a letter like "I", as
-            // opposed to the top of a letter like "e"), which makes calculating positions much
-            // simpler!
-            displayText.setTextOrigin(VPos.TOP);
-            displayText.setFont(Font.font(fontName, fontSize));
-
-            // All new Nodes need to be added to the root in order to be displayed.
-            root.getChildren().add(displayText);
-        }
-
-        private void Display(LinkedList<String> al){
-            String Text = String.join("",al);
-            displayText.setText(Text);
-        }
-
-        @Override
-        public void handle(KeyEvent keyEvent) {
-            if (keyEvent.getEventType() == KeyEvent.KEY_TYPED) {
-                // Use the KEY_TYPED event rather than KEY_PRESSED for letter keys, because with
-                // the KEY_TYPED event, javafx handles the "Shift" key and associated
-                // capitalization.
-
-
-                String characterTyped = keyEvent.getCharacter();
-
-
-                if (characterTyped.length() > 0 && characterTyped.charAt(0) != 8) {
-                    // Ignore control keys, which have non-zero length, as well as the backspace
-                    // key, which is represented as a character of value = 8 on Windows.
-                    allToDisplay.add(characterTyped);
-                    Display(allToDisplay);
-                    keyEvent.consume();
-                }else if (characterTyped.length() > 0 && characterTyped.charAt(0) == 8) {
-                    // Ignore control keys, which have non-zero length, as well as the backspace
-                    // key, which is represented as a character of value = 8 on Windows.
-                    allToDisplay.removeLast();
-                    Display(allToDisplay);
-                }
-
-                centerText();
-            } else if (keyEvent.getEventType() == KeyEvent.KEY_PRESSED) {
-                // Arrow keys should be processed using the KEY_PRESSED event, because KEY_PRESSED
-                // events have a code that we can check (KEY_TYPED events don't have an associated
-                // KeyCode).
-                KeyCode code = keyEvent.getCode();
-                if (code == KeyCode.UP) {
-                    fontSize += 5;
-                    displayText.setFont(Font.font(fontName, fontSize));
-                    centerText();
-                } else if (code == KeyCode.DOWN) {
-                    fontSize = Math.max(0, fontSize - 5);
-                    displayText.setFont(Font.font(fontName, fontSize));
-                    centerText();
-                }
-            }
-        }
-
-        private void centerText() {
-            // Figure out the size of the current text.
-            double textHeight = displayText.getLayoutBounds().getHeight();
-            double textWidth = displayText.getLayoutBounds().getWidth();
-
-            // Calculate the position so that the text will be centered on the screen.
-            double textTop = 0;//textCenterY - textHeight / 2;
-            double textLeft = MARGIN;//textCenterX - textWidth / 2;
-
-            // Re-position the text.
-            displayText.setX(textLeft);
-            displayText.setY(textTop);
-
-            // Make sure the text appears in front of any other objects you might add.
-            displayText.toFront();
-        }
-    }
 
     @Override
     public void start(Stage primaryStage) {
