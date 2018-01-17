52f50ea75c120079816df43af69946f7d7d33a80
Admin <you@example.com>
2018-01-17 17:32:10 -0600

    finish Percolation.java structure

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,7 +1,32 @@
 package hw2;                       
-
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    public Percolation(int N){
 
+    }
+
+    public  void open(int row, int col){
+
+    }
+
+    public boolean isOpen(int row, int col){
+
+    }
+
+    public boolean isFull(int row, int col){
+
+    }
+
+    public int numberOfOpenSites(){
+
+    }
+
+    public boolean percolates(){
+
+    }
+    /***
+    public static void main(String[] args){
+
+    }*/
 }                       
