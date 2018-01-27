eadc9c7ad1014f8f5b8528384f74f188091cbcd1
SJ <sguo.ece@gmail.com>
2018-01-27 01:03:58 -0600

    finish SimpleOomega HashTable Visualizer

--- hw3/hw3/hash/HashTableVisualizer.java
+++ hw3/hw3/hash/HashTableVisualizer.java
@@ -10,9 +10,13 @@ public class HashTableVisualizer {
            N:     number of items
            M:     number of buckets */
 
-        double scale = 1.0;
+        /* double scale = 1.0;
         int N = 50;
-        int M = 10;
+        int M = 10; */
+
+        double scale = 0.5;
+        int N = 2000;
+        int M = 100;
 
         HashTableDrawingUtility.setScale(scale);
         Set<Oomage> oomies = new HashSet<Oomage>();
@@ -24,13 +28,23 @@ public class HashTableVisualizer {
 
     public static void visualize(Set<Oomage> set, int M, double scale) {
         HashTableDrawingUtility.drawLabels(M);
-
+        int bucketsStartPos = 1;
+        int bucketsPosDis = 1;
+        int[] bucketsSize = new int[M];
         /* TODO: Create a visualization of the given hash table. Use
            du.xCoord and du.yCoord to figure out where to draw
            Oomages.
-         */
+        */
+        for (Oomage o : set){
+            int bucketsNum = (o.hashCode() & 0x7FFFFFFF) % M;
+            bucketsSize[bucketsNum] += 1;
+            o.draw(HashTableDrawingUtility.xCoord(bucketsStartPos
+              + bucketsPosDis * bucketsSize[bucketsNum]), HashTableDrawingUtility.yCoord(bucketsNum,M), scale);
+        }
+
+
 
         /* When done with visualizer, be sure to try 
-           scale = 0.5, N = 2000, M = 100. */           
+           scale = 0.5, N = 2000, M = 100. */          
     }
 } 
